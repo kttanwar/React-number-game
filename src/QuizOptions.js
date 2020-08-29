4d134914c598ba617b238527d14af9651c2c47cf
Konark Tanwar <kttanwar35@gmail.com>
2020-08-29 14:20:39 +0530

    Moving the options to the child component

---
+++ src/QuizOptions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuizOptions extends Component {
+    render() {
+        return (
+            <div className="fields">
+                <div className="field-block">10</div>
+            </div>
+        );
+    }
+}
+
+export default QuizOptions;
